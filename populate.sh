505cad9a35c15824f125ecd93da6b822d96a3b72
Connor Glosser <glosser.ca@gmail.com>
2013-03-13 16:36:24 -0400

    Fixed populate bug (didn't search for dotfiles)

--- populate.sh
+++ populate.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 # Automatically create symbolic links to version-controlled files
 
+Escape="\033";
+RedF="${Escape}[31m"; GreenF="${Escape}[32m";
+Reset="${Escape}[0m";
+
 DOTFILE_DIR=`pwd`
 DOTFILES="conkyrc gitconfig gvimrc nethackrc vimrc zshrc"
 
 for file in $DOTFILES; do
-    if [ -L $HOME/$file ]; then
-        echo "[Symbolic Link] : $HOME/$file exists, skip.";
+    TARGET="$HOME/.$file"
+    if [ -L $TARGET ]; then
+        echo "[${RedF}Symlink${Reset}] : Link $TARGET already exists, skipped.";
         # uncomment below line to remove all existing symbolic links
         # rm $HOME/$file
-    elif [ -f $HOME/$file ] || [ -d $HOME/$file ]; then
-        echo "[File or Dir] : $Home/$rcfile exists, skip.";
+    elif [ -f $TARGET ]; then
+        echo "[${RedF}File${Reset}] : File $TARGET already exists, skipped.";
     else
-        LINKNAME=".$file" 
-        ln -sf $DOTFILE_DIR/$file $HOME/$LINKNAME
-        echo "[good] : built symbolic link: $DOTFILE_DIR/$file";
+        ln -sf $DOTFILE_DIR/$file $TARGET
+        echo "[${GreenF}Good${Reset}] : built symbolic link: $TARGET";
     fi
 done
