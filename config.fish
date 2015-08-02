b88d575956a2b30ba37a0b772107d177cc855201
Connor Glosser <glosser1@gmail.com>
2015-08-02 11:48:49 -0400

    Port aliases from zshrc to Fish

--- config.fish
+++ config.fish
@@ -1,1 +1,17 @@
 set -x TERM xterm-256color
+
+function pbcopy
+  xsel --clipboard --input $argv
+end
+
+function pbpaste
+  xsel --clipboard --output $argv
+end
+
+function getwd
+  pwd | tr -d "\n" | pbcopy
+end
+
+function open
+  xdg-open $argv
+end
